f88c2b909f81905da0f157a777d50fc334a0264a
fpaboim <1982065+fpaboim@users.noreply.github.com>
2022-02-16 01:53:37 -0300

    updated packages, now minimally working

--- packages/server-core/db/mongo.js
+++ packages/server-core/db/mongo.js
@@ -1,19 +1,17 @@
 import chalk from "chalk"
-import mongoose from "mongoose"
 
-const connectToDB = async () => {
+const connectToDB = async (mongoose) => {
   let db
 
   console.log("Setup mongodb")
   mongoose.Promise = global.Promise
-  mongoose.set("useNewUrlParser", true)
-  mongoose.set("useFindAndModify", false)
-  mongoose.set("useCreateIndex", true)
-  mongoose.set("useUnifiedTopology", true)
+  // mongoose.set("useNewUrlParser", true)
+  // mongoose.set("useFindAndModify", false)
+  // mongoose.set("useCreateIndex", true)
+  // mongoose.set("useUnifiedTopology", true)
 
   db = await mongoose.connect(process.env.MONGO_URI, {
-    "bufferCommands": false,
-    "bufferMaxEntries": 0
+    "bufferCommands": false
   })
 
   return db
